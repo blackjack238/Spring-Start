d1303f08d1a73cb704cbd523b13d23ad30bca56b
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-08-04 15:39:52 +0300

    only spring boot

--- src/main/java/com/example/demo/entity/Note.java
+++ src/main/java/com/example/demo/entity/Note.java
@@ -1,22 +1,49 @@
 package com.example.demo.entity;
 
-
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-
-
 public class Note {
-    long id;
-    String title;
-    String content;
 
-    public Note(int id, String title, String content) {
-        this.id = id;
+    private long id;
+    private String title;
+    private String content;
+
+    public Note() {
+    }
+
+    public Note(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
