8017b3383120c5846be88f0b401df7ae96915f3d
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-08-14 18:22:10 +0300

    add Spring MVC controller and html templates

---
+++ src/main/java/com/example/demo/controllers/NoteController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entity.Note;
+import com.example.demo.service.NoteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/note")
+public class NoteController {
+
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/list")
+    public String listNotes(Model model) {
+        model.addAttribute("notes", noteService.listAll());
+        return "list_notes";
+    }
+
+    @GetMapping("/edit")
+    public String editNote(@RequestParam long id, Model model) {
+        Note note = noteService.getById(id);
+        model.addAttribute("note", note);
+        return "edit_note";
+    }
+
+    @PostMapping("/edit")
+    public String saveEditedNote(@ModelAttribute Note note) {
+        noteService.update(note);
+        return "redirect:/note/list";
+    }
+
+    @PostMapping("/delete")
+    public String deleteNote(@RequestParam long id) {
+        noteService.deleteById(id);
+        return "redirect:/note/list";
+    }
+}
