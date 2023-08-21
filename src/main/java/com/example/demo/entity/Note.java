547ffe07561768fd5ccdc91105342e822b82f8ec
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-08-21 11:19:24 +0300

    make some changes

--- src/main/java/com/example/demo/entity/Note.java
+++ src/main/java/com/example/demo/entity/Note.java
@@ -1,11 +1,9 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "notes")
 public class Note {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
