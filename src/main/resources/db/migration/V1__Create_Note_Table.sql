d79c9ab9cc56940dd959639352f49b3c5d21a1de
blackjack238 <118196030+blackjack238@users.noreply.github.com>
2023-08-21 23:27:21 +0300

    make some changes 2

--- src/main/resources/db/migration/V1__Create_Note_Table.sql
+++ src/main/resources/db/migration/V1__Create_Note_Table.sql
@@ -3,3 +3,19 @@ CREATE TABLE note (
     title VARCHAR(255),
     content TEXT
 );
+
+
+-- Створення таблиці users
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    enabled BOOLEAN NOT NULL
+);
+
+-- Створення таблиці authorities
+CREATE TABLE authorities (
+    username VARCHAR(255) NOT NULL,
+    authority VARCHAR(255) NOT NULL,
+    FOREIGN KEY (username) REFERENCES users(username)
+);
